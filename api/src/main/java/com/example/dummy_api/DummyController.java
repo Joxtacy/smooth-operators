15932130105958b35b8fc879f12a0702ef7c1d58
daniel.langas <daniel.langas@telavox.com>
2025-08-27 14:35:11 +0200

    dockerfile added

--- api/src/main/java/com/example/dummy_api/DummyController.java
+++ api/src/main/java/com/example/dummy_api/DummyController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api")
@@ -25,5 +27,40 @@ public class DummyController {
     private long fibonacci(int n) {
         if (n <= 1) return n;
         return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    @GetMapping("/check-status")
+    public String checkStatus(@RequestParam String status) {
+        // Validate input
+        if (!status.equals("Assigned") && !status.equals("Unassigned") && !status.equals("Unknown")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // Validate input
+        Status enumStatus = null;
+        for (Status s : Status.values()) {
+            if (s.getStatus().equalsIgnoreCase(status)) {
+                enumStatus = s;
+                break;
+            }
+        }
+
+        return "Received status: " + enumStatus.getStatus();
+    }
+}
+
+enum Status {
+    ASSIGNED("Assigned"),
+    UNASSIGNED("Unassigned"),
+    EMPTY("Unknown");
+
+    private final String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
     }
 }
