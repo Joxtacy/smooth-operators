48096119910b2ee51aefefe7edfc3eef06b89ded
daniel.langas <daniel.langas@telavox.com>
2025-08-27 12:02:49 +0200

    dockerfile added

--- api/src/main/java/com/example/dummy_api/DummyController.java
+++ api/src/main/java/com/example/dummy_api/DummyController.java
@@ -3,6 +3,7 @@ package com.example.dummy_api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api")
